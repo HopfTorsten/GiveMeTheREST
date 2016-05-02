6663ce56f3863078ea36661dce81cce3e86456a2
Moritz Ragg <moritz.ragg@googlemail.com>
2016-05-02 21:04:19 +0200

    Initial GUI

---
+++ GiveMeTheRESTClient/GiveMeTheRESTClient/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace GiveMeTheRESTClient
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Message> messages { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+            Message message3 = new Message("Nero", "And I'll burn all down!", "3.1.2016", new List<Message> { });
+            Message message2 = new Message("Brutus", "I'll kill you!", "2.1.2016", new List<Message> {message3 });
+            Message message1 = new Message("Julius Caesar", "Hay folks!", "1.1.2016", new List<Message> { message2});
+
+            Message message = new Message("Julius Caesar", "Hay folks!", "1.1.2016", new List<Message> { message2 });
+
+            messages = new List<Message>();
+            messages.Add(message1);
+            messages.Add(message);
+
+        }
+
+        public void NewAnswer_Click(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        public void NewComment_Click(object sender, EventArgs e)
+        {
+            // TODO
+        }
+    }
+}
---
+++ GiveMeTheRESTClient/GiveMeTheRESTClient/Message.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace GiveMeTheRESTClient
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public string Date { get; set; }
+        public List<Message> Childs { get; set; }
+
+        public Message(string name, string value, string date, List<Message> childs, int id = 0)
+        {
+            Name = name;
+            Value = value;
+            Date = date;
+            Childs = childs;
+            Id = id;
+        }
+    }
+}
