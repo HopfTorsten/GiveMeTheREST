ef65c79ec2a0aab0903e651b1adb688130d0e2d2
Torsten Hopf <i13009@hb.dhbw-stuttgart.de>
2016-05-03 10:25:28 +0200

    Hibernate Libraries hinzugefügt GiveMeAMessage implementiert GiveMeAMessage annotiert

--- GiveMeTheRESTServer/src/de/dhbw/give/me/the/model/GiveMeAMessage.java
+++ GiveMeTheRESTServer/src/de/dhbw/give/me/the/model/GiveMeAMessage.java
@@ -1,5 +1,59 @@
 package de.dhbw.give.me.the.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "GiveMeAMessage")
 public class GiveMeAMessage {
 
+	@Id @GeneratedValue
+	@Column(name ="id")
+	private Integer id;
+	
+	@Column(name = "parent")
+	private Integer parent;
+	
+	@Column(name = "content")
+	@NotNull
+	private String content;
+	
+	public GiveMeAMessage(){
+		
+	}
+
+	public GiveMeAMessage(Integer parent, String content) {
+		super();
+		this.parent = parent;
+		this.content = content;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getParent() {
+		return parent;
+	}
+
+	public void setParent(Integer parent) {
+		this.parent = parent;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
 }
--- GiveMeTheRESTServer/src/de/dhbw/give/me/the/model/GiveMeTheRestInterface.java
+++ GiveMeTheRESTServer/src/de/dhbw/give/me/the/model/GiveMeTheRestInterface.java
@@ -1,5 +1,6 @@
 package de.dhbw.give.me.the.model;
 import java.util.List;
+
 public interface GiveMeTheRestInterface {
 
 	public List<GiveMeAMessage> getRootMessages();
